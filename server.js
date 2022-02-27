636e8b13153cfadb880d5ef391d6f4a21411f004
mohy <mohy@gmail.com>
2022-02-28 01:22:40 +0200

    make it1245

--- server.js
+++ server.js
@@ -119,8 +119,8 @@ app.put("/image", (req, res) => {
 //     console.log("running");
 // })
 
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
+const server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+const server_host = process.env.YOUR_HOST || '0.0.0.0';
 app.listen(server_port, server_host, function() {
     console.log('Listening on port %d', server_port);
 });
