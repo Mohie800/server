7b3b4d1ad3a0711fdeaf9c84beb1cd26fb891f5a
mohy <mohy@gmail.com>
2022-03-01 00:38:29 +0200

    0.

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import bcrypt from "bcryptjs";
 // import myknex from "./knex/knex.js";
-import { knex, Knex } from 'knex'
+import pkg from 'knex';
+const { knex, Knex } = pkg;
 
 // const knex = require('./knex/knex.js').default;
 // const { Client } = require('pg');
