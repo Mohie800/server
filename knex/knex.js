b25c869b3e4e4c104266e1d57956687a07869437
mohy <mohy@gmail.com>
2022-02-28 23:42:51 +0200

    4

--- knex/knex.js
+++ knex/knex.js
@@ -1,10 +1,10 @@
-// const environment = process.env.ENVIRONMENT || 'development'
-// const config = require('../knexfile.js')[environment];
-// export default require('knex')(config);
+const environment = process.env.ENVIRONMENT || 'development'
+const config = require('../knexfile.js')[environment];
+export default require('knex')(config);
 
-import knex from 'knex'
-import configuration from '../knexfile.js'
+// import knex from 'knex'
+// import configuration from '../knexfile.js'
 
-const Knex = knex(configuration.development)
+// const Knex = knex(configuration.development)
 
-export default knex
+// export default knex
--- knexfile.js
+++ knexfile.js
@@ -3,39 +3,45 @@
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
-export const development = {
-  client: 'pg',
-  connection: {
-    connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false },
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
   }
-};
-export const staging = {
-  client: 'postgresql',
-  connection: {
-    database: 'my_db',
-    user: 'username',
-    password: 'password'
-  },
-  pool: {
-    min: 2,
-    max: 10
-  },
-  migrations: {
-    tableName: 'knex_migrations'
-  }
-};
-export const production = {
-  client: 'pg',
-  connection: {
-    connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false },
-  },
-  pool: {
-    min: 2,
-    max: 10
-  },
-  migrations: {
-    tableName: 'knex_migrations'
-  }
+
 };
